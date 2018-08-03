3cbeb190a5dca63d4036246b136afc7dd990b29d
seni1 <seni@bestpick.ru>
2018-08-03 13:59:45 +0300

    Content Provider Query Method applied

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -89,15 +89,15 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_WEIGHT,
         };
 
-        //Set the Cursor
-        Cursor cursor = db.query(
-                PetEntry.TABLE_NAME,
-                projection,
-                null,
-                null,
-                null,
-                null,
-                null);
+        // Perform a query on the provider using the ContentResolver.
+        // Use the {@link PetEntry#CONTENT_URI} to access the pet data.
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI,  // The content URI of the pets table
+                projection,            // The columns to return for each row
+                null,         // Selection criteria
+                null,      // Selection criteria
+                null);        // The sort order for the returned rows
+
 
 
         TextView displayView = (TextView) findViewById(R.id.text_view_pet);
